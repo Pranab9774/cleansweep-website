8b72d95ae878fff740b37b27eff93a69153dd0e0
Pranab Nath <pranab.nath9@outlook.com>
2026-07-02 19:29:01 +0530

    Delete main.js

--- main.js
+++
@@ -1,4 +0,0 @@
-document.querySelectorAll('#year').forEach(el => el.textContent = new Date().getFullYear());
-const menuButton = document.querySelector('.menu-button');
-const nav = document.querySelector('.nav');
-if (menuButton && nav) menuButton.addEventListener('click', () => nav.classList.toggle('open'));
